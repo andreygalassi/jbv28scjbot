4ef605f5cb7f917f9a3a3f7e071a3254a9193744
andreygalassi <andreygalassi@gmail.com>
2016-11-15 18:28:55 -0200

    Criação da classe utilitaria para controle das entidades em memória

---
+++ jbv28scjbot/src/main/java/br/com/fiap/t28scj/fundamentos/jbv28scjbot/dao/DaoUtils.java
@@ -0,0 +1,10 @@
+package br.com.fiap.t28scj.fundamentos.jbv28scjbot.dao;
+
+/**
+ * Classe utilitária para controle das entidades em memoria
+ * @author Andrey
+ * @since 15/11/2016
+ */
+public class DaoUtils {
+
+}
