802b16a940e4a43df28152d97f4780521e16e9eb
mateus <mateus@mateus-Inspiron-5548>
2016-11-20 02:47:03 -0200

    criação conta

--- jbv28scjbot/src/main/java/br/com/fiap/t28scj/fundamentos/jbv28scjbot/entity/Conta.java
+++ jbv28scjbot/src/main/java/br/com/fiap/t28scj/fundamentos/jbv28scjbot/entity/Conta.java
@@ -12,8 +12,8 @@ import java.util.Set;
  * @author Andrey
  * @since 15/11/2016
  */
-public abstract class Conta {
-	
+public class Conta {
+
 	private Long id;
 	private Pessoa titular;
 	private Set<Pessoa> dependentes;
@@ -21,7 +21,7 @@ public abstract class Conta {
 	private BigDecimal saldo;
 	private List<Movimentacao> movimentacoes;
 	private TipoConta tipo;
-	
+
 	public Conta(Pessoa titular, TipoConta tipo) {
 		super();
 		this.titular = titular;
@@ -30,51 +30,71 @@ public abstract class Conta {
 		this.movimentacoes = new ArrayList<>();
 		this.dependentes = new HashSet<>();
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public Pessoa getTitular() {
 		return titular;
 	}
+
 	public Set<Pessoa> getDependentes() {
+		if(dependentes == null)
+			dependentes = new HashSet<>();
 		return dependentes;
 	}
+
 	public void setDependentes(Set<Pessoa> dependentes) {
 		this.dependentes = dependentes;
 	}
+
 	public LocalDate getDtRegistro() {
 		return dtRegistro;
 	}
+
 	public void setDtRegistro(LocalDate dtRegistro) {
 		this.dtRegistro = dtRegistro;
 	}
+
 	public BigDecimal getSaldo() {
 		return saldo;
 	}
+	
+	public void depositar(BigDecimal valor)
+	{
+		saldo = saldo.add(valor);
+	}
+	
+	public void sacar(BigDecimal valor)
+	{
+		saldo = saldo.subtract(valor).subtract(new BigDecimal(2.5));
+	}
+
 	public List<Movimentacao> getMovimentacoes() {
 		return movimentacoes;
 	}
+
 	public void setMovimentacoes(List<Movimentacao> movimentacoes) {
 		this.movimentacoes = movimentacoes;
 	}
+
 	public TipoConta getTipo() {
 		return tipo;
 	}
+
 	public void setTipo(TipoConta tipo) {
 		this.tipo = tipo;
 	}
 
 	@Override
 	public String toString() {
-		return String.format("Conta [id=%s, titular=%s, qtDependentes=%s, dtRegistro=%s, saldo=%s, tipo=%s]", id, titular,
-				dependentes.size(), dtRegistro, saldo, tipo);
+		return String.format("Conta [id=%s, titular=%s, qtDependentes=%s, dtRegistro=%s, saldo=%s, tipo=%s]", id,
+				titular, dependentes.size(), dtRegistro, saldo, tipo);
 	}
-	
-	
-	
-	
+
 }
